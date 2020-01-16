62fb947c51c7b5758d06dd0a3d1d1bd695982a58
hernangonzalez <hernan@indeba.com>
2020-01-16 18:26:23 +0100

    Initial Commit

---
+++ Marvel/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Marvel
+//
+//  Created by Hernan G. Gonzalez on 16/01/2020.
+//  Copyright © 2020 Hernan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
