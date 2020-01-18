3047aee3be4498d16a131f1383fcff64bb6a461b
hernangonzalez <hernan@indeba.com>
2020-01-19 00:03:33 +0100

    Fade in thumbnail image

--- Marvel/Scenes/Main/Components/HeroCell.swift
+++ Marvel/Scenes/Main/Components/HeroCell.swift
@@ -16,24 +16,26 @@ struct HeroCellViewModel: Equatable {
 class HeroCell: UICollectionViewCell {
     // MARK: Constants
     static let height: CGFloat = 144
-    
+
     // MARK: Dependencies
     var imageLoader: ImageProvider?
 
     // MARK: Properties
-    private weak var thumbnail: UIImageView!
+    private weak var thumbnailView: UIImageView!
     private weak var nameLabel: UILabel!
     private var viewModel: HeroCellViewModel?
 
     // MARK: - Init
     override init(frame: CGRect) {
         super.init(frame: frame)
+        contentView.backgroundColor = .systemRed
         contentView.clipsToBounds = true
 
         let thumb = UIImageView(frame: .zero)
         thumb.translatesAutoresizingMaskIntoConstraints = false
         thumb.backgroundColor = .systemRed
         thumb.contentMode = .scaleAspectFill
+        thumb.alpha = 0
         contentView.addSubview(thumb)
 
         let name = UILabel(frame: .zero)
@@ -59,7 +61,7 @@ class HeroCell: UICollectionViewCell {
         ])
 
         nameLabel = name
-        thumbnail = thumb
+        thumbnailView = thumb
     }
 
     required init?(coder: NSCoder) {
@@ -70,16 +72,26 @@ class HeroCell: UICollectionViewCell {
     override func prepareForReuse() {
         super.prepareForReuse()
         nameLabel.text = nil
-        thumbnail.image = nil
+        thumbnailView.image = nil
+        thumbnailView.alpha = 0
     }
 
     func update(with model: HeroCellViewModel) {
         viewModel = model
         nameLabel.text = model.name
         imageLoader?.image(with: model.thumbnail) { [weak self] image, url in
-            if self?.viewModel?.thumbnail == url {
-                self?.thumbnail.image = image
-            }
+            self?.update(thumbnail: image, url: url)
+        }
+    }
+
+    private func update(thumbnail: UIImage, url: URL) {
+        guard viewModel?.thumbnail == url else {
+            return
+        }
+
+        UIView.animate(withDuration: 0.35) {
+            self.thumbnailView.alpha = 1
+            self.thumbnailView.image = thumbnail
         }
     }
 }
